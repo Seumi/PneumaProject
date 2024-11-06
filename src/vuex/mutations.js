9c26d15bc482f9a39b5dae13d676f13cd91f9138
Seumi <seumi@qq.com>
2024-11-06 20:56:48 +0800

    Update default username to user001

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -2,8 +2,8 @@ const mutations = {
     LOGIN(state, data) {
         state.token = data.token;
         window.sessionStorage.setItem('token', data.token);
-        state.username = data.username;
-        window.sessionStorage.setItem('username', data.username);
+        state.username = 'user001';
+        window.sessionStorage.setItem('username', 'user001');
         state.userID = data.userID;
         window.sessionStorage.setItem('userID', data.userID);
     },
